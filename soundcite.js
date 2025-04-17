ee5b1ad4c1b28cf44ebe99f0d9c1c44b349ab252
mbmotorcyclesproject <myrtlebeachmotorcycleproject@gmail.com>
2025-04-17 13:10:17 -0400

    Create soundcite.js

---
+++ soundcite.js
@@ -0,0 +1,1 @@
+<link href='https://cdn.knightlab.com/libs/soundcite/latest/css/player.css' rel='stylesheet' type='text/css'><script type='text/javascript' src='https://cdn.knightlab.com/libs/soundcite/latest/js/soundcite.min.js'></script>
